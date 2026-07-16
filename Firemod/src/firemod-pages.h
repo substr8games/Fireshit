abf9dcb9166dabecb0628abe1dee9dfe68f6953e
substr8games <philabsresearch@gmail.com>
2026-07-15 20:00:47 -0700

    Delete Firemod directory

--- Firemod/src/firemod-pages.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIREMOD_PAGES_H
-#define FIREMOD_PAGES_H
-
-#include "firemod-app.h"
-
-void firemod_pages_render(FiremodApp *app);
-
-#endif
--- Firemod/src/firemod-parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef FIREMOD_PARSE_H
-#define FIREMOD_PARSE_H
-
-#include "firemod-model.h"
-
-FiremodAuthorityType firemod_parse_type_for_path(const char *path);
-void firemod_parse_authority(FiremodAuthority *authority);
-
-#endif
